abda35224ae656ac0054c7742cc8c25953be6b72
Ryuuz02 <ryuuz02@outlook.com>
2022-01-10 15:46:00 -0600

    Can now properly search and use text files to store information. Also can login now.

--- Login/Login.py
+++ Login/Login.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import kivy
 from kivy.app import App
 from kivy.lang import Builder
@@ -5,7 +7,6 @@ from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.textinput import TextInput
 
 kivy.require("2.0.0")
-
 
 Builder.load_string("""
 <HomeScreen>:
@@ -25,13 +26,13 @@ Builder.load_string("""
         TextInput:
             hint_text: "Enter Username Here"
             multiline: False
-            on_text_validate: print(self.text)
+            on_text_validate: root.find_username(self.text)
         Label:
             text: "Enter Password"
         TextInput:
             hint_text: "Enter Password Here"
             multiline: False
-            on_text_validate: print(self.text)
+            on_text_validate: root.find_password(self.text)
         Button:
             text: "Return To Home Screen"
             on_press: root.manager.current = "Home"
@@ -63,23 +64,60 @@ Builder.load_string("""
 """)
 
 
+def search_file(searched_text, login_type):
+    with open("login_info.txt", "r") as f:
+        with open("login_info.txt", "r") as g:
+            file_length = len(g.read())
+            g.seek(0)
+            file_length += (len(g.readlines()) - 1)
+        found = False
+        while not found:
+            next_line = f.readline()
+            if next_line[0:8] == login_type:
+                iterated_name = next_line[9:len(next_line) - 1]
+                if searched_text.lower() == iterated_name.lower():
+                    found = True
+            if f.tell() == file_length:
+                break
+    return found
+
+
 class HomeScreen(Screen):
     pass
 
 
 class LoginScreen(Screen):
     def find_username(self, obj):
-        pass
+        found = search_file(obj, "username")
+        if found:
+            print("Found your Username")
+        else:
+            print("Invalid Username")
+
+    def find_password(self, obj):
+        found = search_file(obj, "password")
+        if found:
+            print("Found your Password")
+        else:
+            print("Invalid Password")
 
 
 class RegisterScreen(Screen):
     def add_username(self, obj):
-        with open("login_info.txt", "a+") as f:
-            f.write("username: " + obj + "\n")
+        found = search_file(obj, "username")
+        if not found:
+            with open("login_info.txt", "a+") as f:
+                f.write("\nusername:" + obj)
+        else:
+            print("That username is already in use")
 
     def add_password(self, obj):
-        with open("login_info.txt", "a") as f:
-            f.write("password: " + obj + "\n")
+        found = search_file(obj, "password")
+        if not found:
+            with open("login_info.txt", "a+") as f:
+                f.write("\npassword:" + obj)
+        else:
+            print("That username is already in use")
 
 
 class LoginApp(App):
